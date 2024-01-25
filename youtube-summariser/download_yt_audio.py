2759ad920b37ae647b441b5e74780fe5b52d605d
Vince Lam <57117625+vince-lam@users.noreply.github.com>
2024-01-25 10:34:59 +0800

    feat(download): Add progress hooks

--- youtube-summariser/download_yt_audio.py
+++ youtube-summariser/download_yt_audio.py
@@ -4,19 +4,22 @@ import sys
 import yt_dlp
 
 
-def download_audio(youtube_url: str, output_path: str = "outputs/audio"):
-    """
-    Downloads audio from a YouTube video and saves it as a WAV file.
+def download_audio(youtube_url, output_path="outputs/audio"):
+    # List to store downloaded filenames
+    downloaded_filenames = []
 
-    Args:
-        youtube_url (str): The URL of the YouTube video.
-        output_path (str, optional): The path to save the downloaded audio file. Defaults to "outputs/audio".
+    def my_hook(d):
+        if d["status"] == "finished":
+            filename = d["filename"]
+            # Replace the original extension with .wav
+            wav_filename = os.path.splitext(filename)[0] + ".wav"
+            print(f"\n\nDownload complete, now converting:\n{d['filename']}")
+            downloaded_filenames.append(wav_filename)
 
-    Returns:
-        str: The path of the downloaded audio file.
-    """
+    # Ensure the output directory exists
     if not os.path.exists(output_path):
         os.makedirs(output_path)
+
     # Options for downloading audio in best format and converting to wav
     ydl_opts = {
         "format": "bestaudio/best",
@@ -28,27 +31,28 @@ def download_audio(youtube_url: str, output_path: str = "outputs/audio"):
             }
         ],
         "outtmpl": os.path.join(output_path, "%(title)s.%(ext)s"),
+        "progress_hooks": [my_hook],
     }
 
     # Downloading the audio
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-        info_dict = ydl.extract_info(youtube_url, download=True)
-        filename = ydl.prepare_filename(info_dict)
-        # Change the extension to wav, as it's the format we're saving in
-        wav_filename = os.path.splitext(filename)[0] + ".wav"
-        return wav_filename
+        ydl.download([youtube_url])
+
+    # Assuming only one file is downloaded, return the first item in the list
+    return downloaded_filenames[0] if downloaded_filenames else None
+
+
+# Example usage
+# url = "https://youtube.com/watch?v=1O0yazhqaxs" # 5 second video
+"""url = "https://www.youtube.com/watch?v=0NcPkQsKZSQ"  # 10 minute video
+output_file = download_audio(url)
+print(f"Downloaded audio file path: {output_file}")"""
 
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
         youtube_url = sys.argv[1]
         audio_path = download_audio(youtube_url)
-        print(f"Downloaded audio file path: {audio_path}")
+        print(f"\nDownloaded audio file path:\n{audio_path}\n")
     else:
         print("Usage: python -m youtube-summariser.download_yt_audio.py <youtube_url>")
-
-""" TEST
-url = "https://youtube.com/watch?v=1O0yazhqaxs"
-output_file = download_audio(url)
-print(f"Downloaded audio file path: {output_file}")
-"""
