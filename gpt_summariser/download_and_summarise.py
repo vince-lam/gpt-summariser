8879dc6928a6d309103acb77b71af5c14b1e2600
Vince Lam <57117625+vince-lam@users.noreply.github.com>
2024-02-17 11:57:45 +0800

    chore: Rename to summarise_openai

--- gpt_summariser/download_and_summarise.py
+++ gpt_summariser/download_and_summarise.py
@@ -1,8 +1,8 @@
 import sys
 
 from .download_yt_audio import download_audio
+from .summarise_openai import summarise_transcript
 from .transcribe_audio import transcribe
-from .summarise_transcript import summarise_transcript
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
@@ -16,6 +16,4 @@ if __name__ == "__main__":
         )
         summarise_transcript(text_path, title)
     else:
-        print(
-            "Usage: python3 -m gpt_summariser.download_and_summarise <youtube_url> <title>"
-        )
+        print("Usage: python3 -m gpt_summariser.download_and_summarise <youtube_url> <title>")
--- gpt_summariser/summarise_openai.py
+++ gpt_summariser/summarise_openai.py
@@ -13,31 +13,31 @@ from .utils import get_filename_without_file_extension
 load_dotenv()
 
 PROMPT = """
-As a professional transcript summarisier, create a bullet point summary of the
-transcript that follows the delimiter ### TEXT ###. First include a title for the
-text, and then the text itself.
+As a world class transcript summarizer, create a bullet point summary of the
+transcript provided.
 
-The format needs to be in markdown format for logseq.
+First include a suitable title for the summary based on the title within the <TITLE> delimiter.
+Then include the bullet point summary of the text within the <TEXT> delimiter.
 
-Do not just list the general topic, but list the actual facts shared.
+The format of your response needs to be in markdown formatting. Use "- " for bullet points.
+
+Do not just list the general topic or makes things up, only list the actual facts shared.
 
 For example, if a speaker claims that "an increase in X leads to a decrease in
 Y", then you should include that claim in the summary, rather than saying "the
 speaker discussed the relationship between X and Y".
 
-Use "- " for bullet points.
-
-After you have made all bullet points, add one more bullet point that summarises
+After you have made all bullet points, add one more bullet point that summarizes
 the main point of the text. Here is an example:
 
 - Main message: <MAIN MESSAGE HERE>
 
-#######
+######
 
-### TEXT TITLE ###
+<TITLE>
 {title}
 
-### TEXT ###
+<TEXT>
 {chunk}
 """
 
@@ -97,7 +97,6 @@ def summarise(chunk=None, title=None):
     client = openai.Client(api_key=os.getenv("OPENAI_API_KEY"))
     prompt = PROMPT.format(chunk=chunk, title=title)
     print("Prompt sent to OpenAI API.")
-    print(prompt)
     result = client.chat.completions.create(
         model=MODEL,
         messages=[{"role": "user", "content": prompt}],
@@ -160,4 +159,4 @@ if __name__ == "__main__":
         title = sys.argv[2]
         summarise_transcript(text_path, title)
     else:
-        print("Usage: python3 -m gpt_summariser.summarise_transcript <transcript_path> <title>")
+        print("Usage: python3 -m gpt_summariser.summarise_openai <transcript_path> <title>")
